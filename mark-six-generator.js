c21377ba02a63fc31734a362f3077a612e36d2ed
Don Wan <mtwan05@gmail.com>
2026-02-19 17:34:24 +0800

    Add refresh button functionality to generate new numbers

--- mark-six-generator.js
+++ mark-six-generator.js
@@ -1,26 +1,22 @@
-function MarkSixGenerator()
-{
-    this.result = [];
-
-    /**
-     * Generate 6 random numbers and put into result.
-     */
-    this.generate = function () {
-        while (this.result.length < 6) {
-            // Get a random integer from 1 to 49.
-            var num = Math.floor(Math.random() * 49) + 1;
-            // Skip for duplicated number.
-            if (this.result.indexOf(num) >= 0) {
-                continue;
+$(document).ready(function() {
+    // Function to generate numbers
+    function generateNumbers() {
+        const numbers = [];
+        while (numbers.length < 6) {
+            const num = Math.floor(Math.random() * 49) + 1;
+            if (!numbers.includes(num)) {
+                numbers.push(num);
             }
-            this.result.push(num);
         }
+        numbers.sort((a, b) => a - b);
+        $('#result').text(numbers.join(', '));
     }
-}
-
-var generator = new MarkSixGenerator();
-
-$(function() {
-    generator.generate();
-    $('#result').text(generator.result.join(' '));
+    
+    // Generate numbers on page load
+    generateNumbers();
+    
+    // Refresh button click handler
+    $('#refreshBtn').click(function() {
+        generateNumbers();
+    });
 });
